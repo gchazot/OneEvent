b736bd3af691d6962d27cb1f603135f9179a8f6b
Germain Chazot <germain@edited.com>
2020-04-11 15:25:27 +0100

    Cleanup useless things

--- forms.py
+++ forms.py
@@ -1,23 +1,3 @@
-'''
-@author: Germain CHAZOT
-
-Copyright 2014-2015 Germain CHAZOT
-
-This file is part of OneEvent.
-
-OneEvent is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-OneEvent is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with OneEvent.  If not, see <http://www.gnu.org/licenses/>.
-'''
 from django.forms import Form
 from django.forms.fields import ChoiceField, SplitDateTimeField
 from models import Event, Session, Category, Choice, Option, Booking, BookingOption, Message

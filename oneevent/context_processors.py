c2550c82765b5ad64363d637411198d95fb51911
Germain Chazot <germain@edited.com>
2020-05-08 02:10:53 +0100

    Simplify context processor code

--- oneevent/context_processors.py
+++ oneevent/context_processors.py
@@ -5,16 +5,7 @@ def customise_navbar(_request):
     '''
     Defines some site-wide variables for use in oneevent templates
     '''
-    result = {}
-
-    try:
-        result['site_brand'] = settings.ONEEVENT_SITE_BRAND
-    except AttributeError:
-        pass
-
-    try:
-        result['navbar_color'] = settings.ONEEVENT_NAVBAR_COLOR
-    except AttributeError:
-        pass
-
-    return result
+    return {
+        'site_brand': settings.ONEEVENT_SITE_BRAND,
+        'navbar_color': settings.ONEEVENT_NAVBAR_COLOR,
+    }

5f5fe96246727f25978dea9dc55f424de8ee8e2d
lilliputten <lilliputten@yandex.ru>
2024-03-07 21:35:28 +0700

    Issue #1: Added project build variables (verion, timestamps).

--- dds_registration/__init__.py
+++ dds_registration/__init__.py
@@ -1,5 +1,9 @@
 __all__ = (
     "__version__",
+    "__timestamp__",
+    "__timetag__",
 )
 
-__version__ = "0.1"
+__version__ = "0.0.1"
+__timestamp__ = "2024.03.07 21:51 +0700"
+__timetag__ = "240307-2151"
